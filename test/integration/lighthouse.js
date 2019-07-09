957f26bb232324e1623fd0fd303e019e299d1aa2
Steven <steven@ceriously.com>
2019-07-09 16:02:21 -0400

    Enhance lighthouse test

--- test/integration/lighthouse.js
+++ test/integration/lighthouse.js
@@ -1,1 +1,47 @@
-require('lighthouse');
+const chrome = require('chrome-aws-lambda');
+const { launch } = require('puppeteer-core');
+const lighthouse = require('lighthouse');
+const { URL } = require('url');
+
+async function getOptions() {
+  const options = {
+    args: chrome.args,
+    executablePath: await chrome.executablePath,
+    headless: chrome.headless,
+  };
+  return options;
+}
+
+async function getResult(url) {
+  const options = await getOptions();
+  const browser = await launch(options);
+  const { port } = new URL(browser.wsEndpoint());
+  const result = await lighthouse(url, {
+    port,
+    output: 'html',
+    logLevel: 'error',
+  });
+  await browser.close();
+  return result;
+}
+
+async function main() {
+  const result = await getResult('https://zeit.co/about');
+  if (req && result && result.lhr && result.lhr.categories) {
+    console.log('success');
+  } else {
+    console.log('fail');
+  }
+}
+
+getResult('https://zeit.co/about')
+  .then(result => {
+    if (result && result.lhr && result.lhr.categories) {
+      console.log('success');
+    } else {
+      throw new Error('failed to get result')
+    }
+  })
+  .catch(err => {
+    console.error(err)
+  });
