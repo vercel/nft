228a24a97a43c01fcc9583a99acd24d4ed5cc6dc
Steven <steven@ceriously.com>
2019-07-09 16:57:26 -0400

    Remove unused code

--- lighthouse.js
+++
@@ -1,6 +0,0 @@
-const nodeFileTrace = require('./src/node-file-trace');
-const entrypoint = '/Users/styfle/Code/zeit/integrations/lighthouse/lighthouse/index.js';
-const workPath = '/Users/styfle/Code/zeit/integrations/lighthouse';
-nodeFileTrace([entrypoint], { base: workPath })
-  .then(({ fileList, warnings }) => console.log(fileList.filter(f => f.includes('lighthouse'))))
-  .catch(console.error);
