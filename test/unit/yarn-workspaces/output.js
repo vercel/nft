d7c11abef1f3b6d9656c5a879cfc5b00107058d4
Guy Bedford <guybedford@gmail.com>
2019-07-18 08:26:47 -0400

    Handle realpath early in resolveFile (#24)

--- test/unit/yarn-workspaces/output.js
+++ test/unit/yarn-workspaces/output.js
@@ -2,6 +2,6 @@
   "test/unit/yarn-workspaces/input.js",
   "test/unit/yarn-workspaces/node_modules/x",
   "test/unit/yarn-workspaces/packages/x/dep.js",
-  "test/unit/yarn-workspaces/packages/x/main.js",
+  "test/unit/yarn-workspaces/packages/x/main.ts",
   "test/unit/yarn-workspaces/packages/x/package.json"
 ]
