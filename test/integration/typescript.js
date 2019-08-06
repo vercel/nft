d4a43b81034c9fd9df43ca5244a093bfddb6539d
Steven <steven@ceriously.com>
2019-08-06 15:15:36 -0700

    Fix typescript integration test

--- test/integration/typescript.js
+++ test/integration/typescript.js
@@ -2,8 +2,9 @@ const { spawn } = require('child_process');
 const path = require('path');
 
 const tsc = require.resolve('typescript/bin/tsc');
+console.log('path to tsc is ' + tsc);
 
-const child = spawn(tsc, { cwd: path.resolve(__dirname, '../fixtures') });
+const child = spawn(tsc, { cwd: path.resolve(__dirname, '..', 'fixtures') });
 child.stdout.on('data', data => {
   console.error(data.toString());
   throw new Error('Unexpected output.');
