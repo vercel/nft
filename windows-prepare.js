3c22ca61b29db463f72d113376b451eed56f84c5
Steven <steven@ceriously.com>
2019-08-20 13:39:34 -0400

    Exclude argon2 from windows tests

--- windows-prepare.js
+++ windows-prepare.js
@@ -8,15 +8,17 @@ if (process.platform === 'win32') {
   const pkg = JSON.parse(pkgJson);
   unlinkSync(join(__dirname, 'yarn.lock'));
   unlinkSync(join(__dirname, 'test', 'integration', 'tensorflow.js'));
+  unlinkSync(join(__dirname, 'test', 'integration', 'argon2.js'));
   unlinkSync(join(__dirname, 'test', 'integration', 'highlights.js'));
   unlinkSync(join(__dirname, 'test', 'integration', 'hot-shots.js'));
   unlinkSync(join(__dirname, 'test', 'integration', 'yoga-layout.js'));
   unlinkSync(join(__dirname, 'test', 'integration', 'loopback.js'));
   delete pkg.devDependencies['@tensorflow/tfjs-node'];
+  delete pkg.devDependencies['argon2'];
   delete pkg.devDependencies['highlights'];
   delete pkg.devDependencies['hot-shots'];
   delete pkg.devDependencies['yoga-layout'];
   writeFileSync(join(__dirname, 'package.json'), JSON.stringify(pkg));
 } else {
-  console.log('[azure-prepare] Expected current platform to be win32 but found ' + process.platform);
+  console.log('[windows-prepare] Expected current platform to be win32 but found ' + process.platform);
 }
