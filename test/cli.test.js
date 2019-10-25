69cbce93259be4b7f1d8280d088b0b2a3e2ef69c
Steven <steven@ceriously.com>
2019-10-25 11:20:26 -0400

    Fix windows tests backslash to forward slash

--- test/cli.test.js
+++ test/cli.test.js
@@ -3,20 +3,28 @@ const { exec: _exec } = require('child_process');
 const exec = promisify(_exec);
 
 const inputjs = 'unit/wildcard/input.js';
-const outputjs = 'unit/wildcard/assets/asset1.txt'; 
+const outputjs = 'unit/wildcard/assets/asset1.txt';
+
+function normalizeOutput(output) {
+  if (process.platform === 'win32') {
+  // When using Windows, the expected output should use backslash
+    output = output.map(str => str.replace(/\//g, '\\'));
+  }
+  return output;
+}
 
 it('should correctly print trace from cli', async () => {
   const { stderr, stdout } = await exec(`../src/cli.js print ${inputjs}`, { cwd: __dirname });
   if (stderr){
     throw new Error(stderr);
   }
-  expect(stdout).toMatch(outputjs);
+  expect(normalizeOutput(stdout)).toMatch(outputjs);
 });
 
 it('should correctly print trace from required cli', async () => {
-  // This is to satisfy code coverage
+  // This test is only here to satisfy code coverage
   const cli = require('../src/cli.js');
   const files = [__dirname + '/' + inputjs];
   const stdout = await cli('print', files);
-  expect(stdout).toMatch(outputjs);
+  expect(normalizeOutput(stdout)).toMatch(outputjs);
 });
