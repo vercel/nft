94a64aae0a4be8368717852de805f3627381a8e0
Steven <steven@ceriously.com>
2019-11-20 15:23:19 -0500

    Skip bull test when connection string is missing (#83)

--- test/integration/bull.js
+++ test/integration/bull.js
@@ -1,11 +1,12 @@
-const Queue = require('bull');
 const { BULL_REDIS_CONNECTION } = process.env;
 
 if (!BULL_REDIS_CONNECTION) {
   console.log('Skipping bull integration test');
   console.log('Create cache on redislabs.com and export BULL_REDIS_CONNECTION="redis://:password@hostname:port"');
+  return;
 }
 
+const Queue = require('bull');
 const pdfQueue = new Queue('pdf transcoding', BULL_REDIS_CONNECTION);
 
 pdfQueue.process(function(job, done) {
