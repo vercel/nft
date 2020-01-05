f66a7f348dcb4158b6ce0faeab2bb0c17b6266a9
Guy Bedford <guybedford@gmail.com>
2020-01-05 23:48:25 +0200

    Implement simple cache persistence between traces (#94)
    * implement simple cache persistence between traces
    
    * add analysis cache

--- src/node-file-trace.js
+++ src/node-file-trace.js
@@ -45,6 +45,7 @@ class Job {
     ignore,
     log = false,
     mixedModules = false,
+    cache,
   }) {
     base = resolve(base);
     this.ignoreFn = path => {
@@ -72,9 +73,17 @@ class Job {
     this.mixedModules = mixedModules;
     this.reasons = Object.create(null);
 
-    this.fileCache = new Map();
-    this.statCache = new Map();
-    this.symlinkCache = new Map();
+    this.fileCache = cache && cache.fileCache || new Map();
+    this.statCache = cache && cache.statCache || new Map();
+    this.symlinkCache = cache && cache.symlinkCache || new Map();
+    this.analysisCache = cache && cache.analysisCache || new Map();
+
+    if (cache) {
+      cache.fileCache = this.fileCache;
+      cache.statCache = this.statCache;
+      cache.symlinkCache = this.symlinkCache;
+      cache.analysisCache = this.analysisCache;
+    }
 
     this.fileList = new Set();
     this.esmFileList = new Set();
@@ -196,14 +205,22 @@ class Job {
 
     const emitted = this.emitFile(path, 'dependency', parent);
     if (!emitted) return;
-  
     if (path.endsWith('.json')) return;
     if (path.endsWith('.node')) return await sharedlibEmit(path, this);
 
-    const source = this.readFile(path);
-    if (source === null) throw new Error('File ' + path + ' does not exist.');
+    let deps, assets, isESM;
 
-    const { deps, assets, isESM } = await analyze(path, source, this);
+    const cachedAnalysis = this.analysisCache.get(path);
+    if (cachedAnalysis) {
+      ({ deps, assets, isESM } = cachedAnalysis);
+    }
+    else {
+      const source = this.readFile(path);
+      if (source === null) throw new Error('File ' + path + ' does not exist.');
+      ({ deps, assets, isESM } = await analyze(path, source, this));
+      this.analysisCache.set(path, { deps, assets, isESM });
+    }
+
     if (isESM)
       this.esmFileList.add(relative(this.base, path));
     await Promise.all([
