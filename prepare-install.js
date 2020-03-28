d31adad4dc8e1a8bc6d0f206d2710de94b55f982
Guy Bedford <guybedford@gmail.com>
2020-03-27 19:35:52 -0600

    win-only prepare install

--- prepare-install.js
+++ prepare-install.js
@@ -1,16 +1,13 @@
 const { unlinkSync, readFileSync, writeFileSync } = require('fs');
 const { join } = require('path');
 
-const pkgJson = readFileSync(join(__dirname, 'package.json'), 'utf8');
-const pkg = JSON.parse(pkgJson);
 const isWin = process.platform === 'win32';
-const isNode12 = process.version.startsWith('v12.');
-
-if (isWin || isNode12) {
-  unlinkSync(join(__dirname, 'yarn.lock'));
-}
 
 if (isWin) {
+  const pkgJson = readFileSync(join(__dirname, 'package.json'), 'utf8');
+  const pkg = JSON.parse(pkgJson);
+
+  unlinkSync(join(__dirname, 'yarn.lock'));
   // Delete the integration tests that will never work in Windows
   unlinkSync(join(__dirname, 'test', 'integration', 'tensorflow.js'));
   unlinkSync(join(__dirname, 'test', 'integration', 'argon2.js'));
@@ -21,6 +18,6 @@ if (isWin) {
   delete pkg.devDependencies['argon2'];
   delete pkg.devDependencies['highlights'];
   delete pkg.devDependencies['hot-shots'];
-}
 
-writeFileSync(join(__dirname, 'package.json'), JSON.stringify(pkg));
+  writeFileSync(join(__dirname, 'package.json'), JSON.stringify(pkg));
+}
