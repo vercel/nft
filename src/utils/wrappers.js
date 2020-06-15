7886e17f7c51953ddea370e15a597d3e89bcb34c
Guy Bedford <guybedford@gmail.com>
2020-06-14 21:44:30 -0700

    handle null init check

--- src/utils/wrappers.js
+++ src/utils/wrappers.js
@@ -309,7 +309,8 @@ function handleWrappers (ast) {
         wrapper.callee.body.body[0].type === 'VariableDeclaration' &&
         wrapper.callee.body.body[0].declarations.length === 1 &&
         wrapper.callee.body.body[0].declarations[0].type === 'VariableDeclarator' &&
-        wrapper.callee.body.body[0].declarations[0].id.type === 'Identifier' && (
+        wrapper.callee.body.body[0].declarations[0].id.type === 'Identifier' && 
+        wrapper.callee.body.body[0].declarations[0].init && (
           wrapper.callee.body.body[0].declarations[0].init.type === 'ObjectExpression' &&
           wrapper.callee.body.body[0].declarations[0].init.properties.length === 0 ||
           wrapper.callee.body.body[0].declarations[0].init.type === 'CallExpression' &&
