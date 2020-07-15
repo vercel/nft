265dc2ad16b409aa5a6bf7bd05e11b5b0eeac026
Steven <steven@ceriously.com>
2020-07-15 16:10:31 -0400

    Remove incorrect output

--- test/unit/yarn-workspaces/output.js
+++ test/unit/yarn-workspaces/output.js
@@ -2,7 +2,6 @@
   "package.json",
   "test/unit/yarn-workspaces/input.js",
   "test/unit/yarn-workspaces/node_modules/x",
-  "test/unit/yarn-workspaces/node_modules/x/package.json",
   "test/unit/yarn-workspaces/packages/x/dep.js",
   "test/unit/yarn-workspaces/packages/x/main.ts",
   "test/unit/yarn-workspaces/packages/x/package.json"
