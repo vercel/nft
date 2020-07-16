86c238a37601dff08be2dd47d0e578b8a85c2541
Steven <steven@ceriously.com>
2020-07-16 13:47:33 -0400

    Fix ts exports and code coverage (#136)
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,14 @@
 module.exports = {
-  collectCoverageFrom: ["src/**/*.js"],
+  collectCoverageFrom: ["out/**/*.js"],
   coverageReporters: ["html", "lcov"],
+  coverageThreshold: {
+    global: {
+      branches: 68.3,
+      functions: 81.89,
+      lines: 73.4,
+      statements: -364
+    }
+  },
   testEnvironment: "node",
   testMatch: ["<rootDir>/test/*(*.)@(spec|test).js?(x)"]
 };
