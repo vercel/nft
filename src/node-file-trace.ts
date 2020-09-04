f69d3fbe04787812bc0544323f0515b93193ded2
Steven <steven@ceriously.com>
2020-09-03 21:12:19 -0400

    Fix symlinks when `base: "/"`

--- src/node-file-trace.ts
+++ src/node-file-trace.ts
@@ -5,12 +5,14 @@ import analyze, { AnalyzeResult } from './analyze';
 import resolveDependency from './resolve-dependency';
 import { isMatch } from 'micromatch';
 import { sharedLibEmit } from './utils/sharedlib-emit';
+import { join } from 'path';
 
 const { gracefulify } = require('graceful-fs');
 gracefulify(fs);
 
 function inPath (path: string, parent: string) {
-  return path.startsWith(parent) && path[parent.length] === sep;
+  const pathWithSep = join(parent, sep);
+  return path.startsWith(pathWithSep) && path !== pathWithSep;
 }
 
 export async function nodeFileTrace(files: string[], opts: NodeFileTraceOptions = {}): Promise<NodeFileTraceResult> {
