b999f0a05e21a1ad16b119e522244ab7312f1267
Guy Bedford <guybedford@gmail.com>
2021-03-21 22:07:31 +0200

    fixup unit test

--- test/unit/browserify-minify/output.js
+++ test/unit/browserify-minify/output.js
@@ -1,6 +1,6 @@
 [
   "node_modules/acorn/dist/acorn.js",
-  "node-file-trace/node_modules/acorn/package.json",
-  "node-file-trace/package.json",
+  "node_modules/acorn/package.json",
+  "package.json",
   "test/unit/browserify-minify/input.js"
 ]
