76c7807fa1b006e272dee069489ee4abea2e21aa
Rich Harris <hello@rich-harris.dev>
2022-05-17 12:03:39 -0400

    add node: prefix to a unit test

--- test/unit/asset-fs-array-expr/input.js
+++ test/unit/asset-fs-array-expr/input.js
@@ -1,4 +1,4 @@
 const { spawn } = require('child_process');
-const { join } = require('path');
+const { join } = require('node:path');
 
 const child = spawn('gifsicle', ['--colors', '256', join(__dirname, './asset1.txt')]);
