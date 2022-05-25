7d89f2048ac5037f5f1660751819e9736748e3e1
Sean Massa <endangeredmassa@gmail.com>
2022-05-25 17:13:24 -0500

    add test for node-pre-gyp

---
+++ test/integration/node-pre-gyp.js
@@ -0,0 +1,5 @@
+const mapboxNodePreGyp = require('@mapbox/node-pre-gyp')
+
+const nodePreGyp = require('node-pre-gyp')
+const nodePreGypPreBinding = require('node-pre-gyp/lib/pre-binding')
+const nodePreGypPreBindingJs = require('node-pre-gyp/lib/pre-binding.js')
