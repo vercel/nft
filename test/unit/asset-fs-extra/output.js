1aea03452516fda9105892b1f9563eac5b2689d7
Steven <steven@ceriously.com>
2022-07-13 16:56:11 -0400

    Fix json

--- test/unit/asset-fs-extra/output.js
+++ test/unit/asset-fs-extra/output.js
@@ -2,5 +2,5 @@
   "package.json",
   "test/unit/asset-fs-extra/asset1.txt",
   "test/unit/asset-fs-extra/asset2.json",
-  "test/unit/asset-fs-extra/input.js",
-];
+  "test/unit/asset-fs-extra/input.js"
+]
