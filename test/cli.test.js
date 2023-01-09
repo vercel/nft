f018459c44371004ffc8b4eb711df608b88b8559
Matheus Fernandes <matheus.frndes@gmail.com>
2023-01-09 16:52:39 +0000

    Add coverage tests back

--- test/cli.test.js
+++ test/cli.test.js
@@ -57,4 +57,45 @@ it('should correctly print help when unknown action is used', async () => {
   }
   expect(stdout).toMatch('$ nft [command] <file>');
 });
+it('[coverage] should correctly print trace from required cli', async () => {
+  // This test is only here to satisfy code coverage
+  const cli = require('../out/cli.js')
+  const files = join(__dirname, inputjs);
+  const stdout = await cli('print', files);
+  expect(stdout).toMatch(normalizeOutput(outputjs));
+});
 
+it('[coverage] should correctly build dist from required cli', async () => {
+  // This test is only here to satisfy code coverage
+  const cli = require('../out/cli.js')
+  const files = join(__dirname, inputjs);
+  await cli('build', files);
+  const found = existsSync(join(__dirname, outputjs));
+  expect(found).toBe(true);
+});
+
+it('[coverage] should correctly show size in bytes from required cli', async () => {
+  // This test is only here to satisfy code coverage
+  const cli = require('../out/cli.js')
+  const files = join(__dirname, inputjs);
+  const stdout = await cli('size', files);
+  expect(stdout).toMatch('bytes total');
+});
+
+it('[coverage] should correctly show why from required cli', async () => {
+  // This test is only here to satisfy code coverage
+  const cli = require('../out/cli.js')
+  const entrypoint = join(__dirname, inputjs);
+  const exitpoint = join(__dirname, outputjs);
+  const cwd = __dirname;
+  const stdout = await cli('why', entrypoint, exitpoint, 'dist', cwd);
+  expect(stdout.replace(/\\/g, '/')).toMatch('unit/wildcard/assets/asset1.txt\nunit/wildcard/input.js');
+});
+
+it('[coverage] should correctly print help when unknown action is used', async () => {
+  // This test is only here to satisfy code coverage
+  const cli = require('../out/cli.js')
+  const files = join(__dirname, inputjs);
+  const stdout = await cli('unknown', files);
+  expect(stdout).toMatch('$ nft [command] <file>');
+});
