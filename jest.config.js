beea509c385a61d55268ba0cbb5bcce540daa166
Steven <steven@ceriously.com>
2024-02-02 12:58:19 -0500

    update coverage threshold

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
   coverageReporters: ["html", "lcov"],
   coverageThreshold: {
     global: {
-      branches: 87.29,
+      branches: 87.20,
       functions: 96.25,
       lines: 92.33,
       statements: -249
