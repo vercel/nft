e4eab55a89510df09e0e573670969895ffff5afd
Steven <steven@ceriously.com>
2024-02-13 17:09:41 -0500

    change coverage threshold

--- jest.config.js
+++ jest.config.js
@@ -3,9 +3,9 @@ module.exports = {
   coverageReporters: ["html", "lcov"],
   coverageThreshold: {
     global: {
-      branches: 87.29,
+      branches: 87.27,
       functions: 96.25,
-      lines: 92.33,
+      lines: 92.28,
       statements: -249
     }
   },
