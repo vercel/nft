1429669976c6b110f145aa0a557f2b469bd98379
Steven <steven@ceriously.com>
2024-07-03 17:44:54 -0400

    chore(test): change axios test url

--- test/integration/axios.js
+++ test/integration/axios.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 
 (async () => {
   const { data } = await axios({
-    url: "https://dog.ceo/api/breeds/image/random"
+    url: "https://example.vercel.sh"
   });
   if (data.status !== "success") {
     throw new Error("Unexpected response: " + JSON.stringify(data));
