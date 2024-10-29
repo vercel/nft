58ad35f1dfda99c7c34448a54824a738e581c527
Steven <steven@ceriously.com>
2024-10-29 14:38:06 -0400

    newlines

--- test/ecmascript/update-all-data.mjs
+++ test/ecmascript/update-all-data.mjs
@@ -1,11 +1,13 @@
 import { readdir, writeFile } from 'fs/promises';
-const dir = new URL('./', import.meta.url);
 
+const dir = new URL('./', import.meta.url);
 const files = await readdir(dir);
+
 for (const file of files.filter(f => f.startsWith('data-'))) {
   const url = `https://raw.githubusercontent.com/kangax/compat-table/gh-pages/${file}`;
   const res = await fetch(url);
   const text = await res.text();
   await writeFile(new URL(file, dir), text);
 }
+
 console.log('Update complete!')
