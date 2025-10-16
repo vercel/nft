41d55204fb34ae02e1659398c11410e5f401b827
Steven <steven@ceriously.com>
2025-10-16 08:56:47 -0400

    Revert "fix: fs.readFile emit relative assets using cwd (#542)"
    This reverts commit dc4ab0b31b14b6513da9fcf2d6b3d1e2cbf33323.

--- test/unit/fs-emission/output.js
+++ test/unit/fs-emission/output.js
@@ -1,6 +1,5 @@
 [
   "package.json",
-  "test/unit/fs-emission/asset1.txt",
   "test/unit/fs-emission/asset2.txt",
   "test/unit/fs-emission/asset3.txt",
   "test/unit/fs-emission/input.js"
--- test/unit/fs-relative-emission/main/output.js
+++
@@ -1,8 +0,0 @@
-[
-  "package.json",
-  "test/unit/fs-relative-emission/asset1.txt",
-  "test/unit/fs-relative-emission/blog/asset2.txt",
-  "test/unit/fs-relative-emission/blog/asset4.txt",
-  "test/unit/fs-relative-emission/blog/author/asset3.txt",
-  "test/unit/fs-relative-emission/main/input.js"
-]
