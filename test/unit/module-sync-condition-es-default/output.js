391e9b37e3807f5cb3e6f2ab1ed9ddf1a34fe575
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2026-05-20 21:12:06 +0200

    add missing test

---
+++ test/unit/module-sync-condition-es-default/output.js
@@ -0,0 +1,6 @@
+[
+  "test/unit/module-sync-condition-es-default/fallback.js",
+  "test/unit/module-sync-condition-es-default/input.js",
+  "test/unit/module-sync-condition-es-default/module-sync.js",
+  "test/unit/module-sync-condition-es-default/package.json"
+]
