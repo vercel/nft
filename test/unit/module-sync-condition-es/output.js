dd0bd0541a16025177166d745ab79953fb82d518
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2026-05-21 14:23:47 +0200

    feat: module-sync fallback should use import for ESM (#581)
    I found this while implementing the Turbopack version:
    https://github.com/vercel/next.js/pull/93970
    
    When doing an ESM `import`, it used the `module-sync` condition
    (correct) and also included the `default` condition, even if there was a
    `import` condition (which is what older Node.js versions would have
    used)
    ```
      "exports": {
        ".": {
          "module-sync": "./module-sync.js", // included (correct)
          "import": "./import.js", // skipped (incorrect)
          "default": "./fallback.js" // included (incorrect)
        }
      }
    ```

--- test/unit/module-sync-condition-es/output.js
+++ test/unit/module-sync-condition-es/output.js
@@ -1,5 +1,5 @@
 [
-  "test/unit/module-sync-condition-es/fallback.js",
+  "test/unit/module-sync-condition-es/import.js",
   "test/unit/module-sync-condition-es/input.js",
   "test/unit/module-sync-condition-es/module-sync.js",
   "test/unit/module-sync-condition-es/package.json"
