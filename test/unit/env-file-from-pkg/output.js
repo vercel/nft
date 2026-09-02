fbeb94d06c87a171b81bb42390e09f24f16484cc
James Garbutt <43081j@users.noreply.github.com>
2026-09-02 12:23:13 +0100

    fix: revert packageBase constraint of asset emission
    Reverts half of #568.
    
    Closes #607.
    
    Basically, #568 does two things while stating it does one:
    
    1. Ignore any paths outside the job's root
    2. Ignore any paths emitted by a `node_modules` file which sit outside
       that same `node_modules`
    
    These were both introduced to fix some problems with sveltekit including
    things like `/bin` and what not. However, only the first is needed for
    that.
    
    The second change seems unnecessary and breaks a lot of common patterns
    (e.g. the `dotenv` one in the newly added test).

---
+++ test/unit/env-file-from-pkg/output.js
@@ -0,0 +1,7 @@
+[
+  "package.json",
+  "test/unit/env-file-from-pkg/.env",
+  "test/unit/env-file-from-pkg/input.js",
+  "test/unit/env-file-from-pkg/node_modules/some-pkg/index.js",
+  "test/unit/env-file-from-pkg/node_modules/some-pkg/package.json"
+]
